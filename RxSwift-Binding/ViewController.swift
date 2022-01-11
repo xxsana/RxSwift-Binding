c23fca55131c1181f94902c9505ea1704ec9c4ee
sanaJeong-dev <sanaJeong.dev@gmail.com>
2022-01-11 17:54:14 +0900

    Completed

--- RxSwift-Binding/ViewController.swift
+++ RxSwift-Binding/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var todos = [String]()
+    var todosRelay = BehaviorRelay(value: [String]())
+    
+    let disposeBag = DisposeBag()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // 이 화면에 이 컴포넌트에는 이 셀 쓸거라는 뜻, 그래서 클래스랑 아이디 등록
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TodoCell")
+        
+        bindToRx()
     }
-
-
+ 
+    func bindToRx() {
+        // when clicked add button
+        // 클릭 되는 순간마다 next event 발생
+        addButton.rx.tap.subscribe ( onNext: {
+            // if textField is empty, nothing happens
+            guard self.textField.text?.isEmpty == false else {
+                return
+            }
+            
+            self.todos.append(self.textField.text!)
+            self.todosRelay.accept(self.todos)   // for watch change, accept todos on BehaviorRelay
+            self.textField.text = ""
+        }).disposed(by: disposeBag)
+        
+        // when todos has been changed
+        todosRelay
+            .bind(to: tableView.rx.items(cellIdentifier: "TodoCell")) {
+                (index, todo: String, cell: UITableViewCell) in
+                cell.textLabel?.text = todo // set text label
+            }.disposed(by: disposeBag)
+    }
 }
-
